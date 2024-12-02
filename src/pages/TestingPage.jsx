e7eda1af9db703326ceea7be89bef2dc575477d9
Crystal Yap <crystalywj013@gmail.com>
2024-12-02 14:55:43 +0800

    create project, delete project

--- src/pages/TestingPage.jsx
+++ src/pages/TestingPage.jsx
@@ -1,18 +1,28 @@
 import { Box, Button, Container, Flex, Input } from '@chakra-ui/react'
+import {Table,Thead,Tbody,Tfoot,Tr,Th,Td,TableCaption,TableContainer} from '@chakra-ui/react'
 import React from 'react'
 import { useState, useEffect } from 'react'
-import ProjectListTable from '../components/ProjectListTable/ProjectListTable';
 import { supabase } from '../client';
 
 function TestingPage(){
 
   const [ name, setName ] = useState("");
-  const [ description, setDescription ] = useState("");
+  const [ status, setStatus ] = useState("");
+
   const [ projects, setProjects ] = useState([])
 
+  const [ project, setProject ] = useState({
+    name:'',status:''
+  })
+
+  const [ project2, setProject2 ] = useState({
+    id:'',name:'',status:''
+  })
+
+
   console.log(name)
-  console.log(description)
-  console.log(projects)
+  console.log(status)
+  console.log(project2)
 
   useEffect(() => {
     getProjects()
@@ -24,14 +34,58 @@ function TestingPage(){
       .from('projects')
       .select('*')
       setProjects(data)
+  }
 
+  async function createProjects(){
+    const { data, error } = await supabase
+      .from('projects')
+      .insert({
+        name:name,
+        status:status 
+      })
+    
+      getProjects()
+ 
+  }
+
+  function displayProject(projectId){
+
+    projects.map((project)=>{
+      if(project.id==projectId){
+        setProject2({
+          id:project.id,
+          name:project.name,
+          status:project.status
+        })
+      }
+    })
+
+  }
+
+  async function deleteProject(projectId){
+    const { data, error } = await supabase
+      .from('projects')
+      .delete()
+      .eq('id', projectId)
+
+    getProjects()
+
+    if (error){
+      console.log(error)
+    }
+
+    if (data){
+      console.log(data)
+    }
+      
   }
    
   return (
     <>
     <Container>
-      <form >
-       <Flex flexDir={"column"} gap={10}>
+      {/*create project FORM 1*/}
+      <form onSubmit={createProjects}  >
+       <Flex flexDir={"column"} gap={10} m={"0 0 60px"}>
         <Flex flexDir={"column"} gap={3} >
           Project Name
           <Input
@@ -43,28 +97,91 @@ function TestingPage(){
         </Flex>
     
         <Flex flexDir={"column"} gap={3}>
-          Project Description
+          Project Status
           <Input
-            name='projectDescription'
-            placeholder={"add description"}
-            onChange={(e) => setDescription(e.target.value)}
+            name='projectStatus'
+            placeholder={"add status"}
+            onChange={(e) => setStatus(e.target.value)}
             
           />
         </Flex>
         
-        <Button>Create Project</Button>
+        <Button onClick={() => createProjects()}>Create Project</Button>
 
       </Flex> 
       </form>
-      
+
+      {/*create project FORM 2*/}
+      <form onSubmit={updateProject(project2.id)} >
+       <Flex flexDir={"column"} gap={10}>
+        <Flex flexDir={"column"} gap={3} >
+          Project Name
+          <Input
+            name='projectName'
+            defaultValue={project2.name}
+            onChange={(e) => setName(e.target.value)}
+            
+          />
+        </Flex>
+    
+        <Flex flexDir={"column"} gap={3}>
+          Project Status
+          <Input
+            name='projectStatus'
+            defaultValue={project2.status}
+            onChange={(e) => setStatus(e.target.value)}
+            
+          />
+        </Flex>
+        
+        <Button onClick={() => createProjects()}>Save Changes</Button>
+
+      </Flex> 
+      </form>
+
+
+      {/*display project list */}
       <Flex m={"50px 0 0"} flexDir={"column"} gap={10}>
         <Box>
           Current Database Items
         </Box>
         
-          <Box>
-          <ProjectListTable />
-          </Box>
+        <Box border={"1px solid black"} borderRadius={"20"} m={"0"} p={"5px 10px"}>
+          <TableContainer>
+            <Table variant='simple'>
+              <Thead>
+              <Tr>
+                <Th>Id</Th>
+                <Th>Project</Th>
+                <Th>Status</Th>
+                <Th>Assignee</Th>
+                <Th>Due Date</Th>
+                <Th>Priority</Th>
+                <Th>Progress</Th>
+              </Tr>
+              </Thead>
+              <Tbody>
+                {projects.map((project)=>
+                  <Tr>
+                    <Td>{project.id}</Td>
+                    <Td>{project.name}</Td>
+                    <Td>{project.status}</Td>
+                    <Td >{project.assignee}</Td>
+                    <Td>{project.dueDate}</Td>
+                    <Td>{project.priority}</Td>
+                    <Td>{project.progress}</Td>
+                    <Td><Button onClick={()=>{updateProject(project.id)}} >Edit</Button></Td>
+                    <Td><Button onClick={()=>{displayProject(project.id)}} >Delete</Button></Td>
+                  </Tr>
+                )}
+                
+                
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Box>
+    
+  
 
       </Flex>      
     </Container>
